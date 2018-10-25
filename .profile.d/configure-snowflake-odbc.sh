7862af0c2d62ab79ddfec4cd35a82304db8b2519
Michael Bensoussan <michael@drivy.com>
2018-10-25 18:29:50 +0200

    More snowflake ODBC configuration

--- .profile.d/configure-snowflake-odbc.sh
+++ .profile.d/configure-snowflake-odbc.sh
@@ -7,7 +7,10 @@ echo "[snowflake]
 Description=SnowflakeDB
 Driver=SnowflakeDSIIDriver
 Locale=en-US
-SERVER=${SNOWFLAKE_ACCOUNT}.snowflakecomputing.com
+database=${SNOWFLAKE_DATABASE}
+warehouse=${SNOWFLAKE_WAREHOUSE}
+schema=${SNOWFLAKE_SCHEMA}
+SERVER=${SNOWFLAKE_ENDPOINT}
 PORT=443
 SSL=on
 CLIENT_SESSION_KEEP_ALIVE=true
